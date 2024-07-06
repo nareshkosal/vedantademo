9e96793954b9e663adf8953f4fffbd7da92427f5
vicky-dot20 <svigneshkumar110@gmail.com>
2024-07-06 09:51:35 +0530

    demo commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import ImageCarousel from "@/components/Imagecarousel";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,10 @@ export default function Home() {
   return (
    <>
    <Navbar />
-   <ImageCarousel />
+   {/* <ImageCarousel /> */}
+   <div className="absolute bottom-0 w-full">
+   <Footer/>
+   </div>
    </>
   );
 }
