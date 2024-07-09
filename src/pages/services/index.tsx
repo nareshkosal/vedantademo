4cf414ad66946c2512707a3d7f7e81d9c889986c
Dineshsanjiv <dineshsanjiv97@outlook.com>
2024-07-09 14:29:14 +0530

    footer cchanges

---
+++ src/pages/services/index.tsx
@@ -0,0 +1,19 @@
+
+import Header from '@/components/Header/Header'
+import MainContent from '@/components/Maincontent/Maincontent';
+import Footer from "@/components/footer/Footer";
+import React from 'react'
+
+const index = () => {
+    return (
+      <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+      
+      </>
+      
+    )
+  }
+
+export default index;
